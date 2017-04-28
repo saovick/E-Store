d57c2236507d09e4266e81e8f8b3770d3b718fa6
Saovick K Biswas <saovickkbiswas@gmail.com>
2017-04-28 08:08:09 -0700

    first push
    First Push to git

---
+++ e-shop/js/e-shop.js
@@ -0,0 +1,352 @@
+$(document).ready(function(){
+	
+
+$(".starone").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	if("$('.starone').data('clicked') && $('.startwo').css({'color':'#FFD700'}) || $('.starone').data('clicked) && $('.starthr').css({'color':'#FFD700'}) || $('.starone').data('clicked) && $('.starfour').css({'color':'#FFD700'}) || $('.starone').data('clicked) && $('.starfiv').css({'color':'#FFD700'}) ")
+	{
+	  $('.startwo').css({'color':'#000'});	
+	  $('.starthr').css({'color':'#000'});
+	  $('.starfour').css({'color':'#000'});
+	  $('.starfiv').css({'color':'#000'});
+	}
+	
+});
+
+$(".startwo").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	
+	$(this).css({'color':'#FFD700'});
+	$(".starone").css({'color':'#FFD700'});
+	if("$('.startwo').data('clicked) && $('.starthr').css({'color':'#FFD700'} ) || $('.startwo').data('clicked) && $('.starfour').css({'color':'#FFD700'}) || $('.startwo').data('clicked) && $('.starfiv').css({'color':'#FFD700'})" )
+	{
+	  $('.starthr').css({'color':'#000'});	
+	  $('.starfour').css({'color':'#000'});
+	  $('.starfiv').css({'color':'#000'});	
+	}	
+	
+});
+
+$(".starthr").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	$(".starone").css({'color':'#FFD700'});
+	$(".startwo").css({'color':'#FFD700'});
+	if("$('.starthr').data('clicked) && $('.starfour').css({'color':'#FFD700'}) || $('.starthr').data('clicked) && $('.starfiv').css({'color':'#FFD700'}) " )
+	{
+	  $('.starfour').css({'color':'#000'});	
+	  $('.starfiv').css({'color':'#000'});		
+	}	
+});
+
+$(".starfour").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	$(".starone").css({'color':'#FFD700'});
+	$(".startwo").css({'color':'#FFD700'});
+	$(".starthr").css({'color':'#FFD700'});
+	if("$('.starfour').data('clicked) && $('.starfiv').css({'color':'#FFD700'})" )
+	{
+	  $('.starfiv').css({'color':'#000'});	
+		
+	}	
+});
+
+$(".starfiv").on('click',function()	{
+	
+	
+	$(this).css({'color':'#FFD700'});
+	$(".starone").css({'color':'#FFD700'});
+	$(".startwo").css({'color':'#FFD700'});
+	$(".starthr").css({'color':'#FFD700'});
+	$(".starfour").css({'color':'#FFD700'});
+});
+
+
+
+
+//
+
+$(".pnsone").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	if("$('.pnsone').data('clicked) && $('.pnstwo').css({'color':'#FFD700'}) || $('.pnsone').data('clicked) && $('.pnsthr').css({'color':'#FFD700'}) || $('.pnsone').data('clicked) && $('.pnsfour').css({'color':'#FFD700'}) || $('.pnsone').data('clicked) && $('.pnsfiv').css({'color':'#FFD700'}) ")
+	{
+	  $('.pnstwo').css({'color':'#000'});	
+	  $('.pnsthr').css({'color':'#000'});
+	  $('.pnsfour').css({'color':'#000'});
+	  $('.pnsfiv').css({'color':'#000'});
+	}
+	
+});
+
+$(".pnstwo").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	
+	$(this).css({'color':'#FFD700'});
+	$(".pnsone").css({'color':'#FFD700'});
+	if("$('.pnstwo').data('clicked) && $('.pnsthr').css({'color':'#FFD700'} ) || $('.pnstwo').data('clicked) && $('.pnsfour').css({'color':'#FFD700'}) || $('.pnstwo').data('clicked) && $('.pnsfiv').css({'color':'#FFD700'})" )
+	{
+	  $('.pnsthr').css({'color':'#000'});	
+	  $('.pnsfour').css({'color':'#000'});
+	  $('.pnsfiv').css({'color':'#000'});	
+	}	
+	
+});
+
+$(".pnsthr").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	$(".pnsone").css({'color':'#FFD700'});
+	$(".pnstwo").css({'color':'#FFD700'});
+	if("$('.pnsthr').data('clicked) && $('.pnsfour').css({'color':'#FFD700'}) || $('.pnsthr').data('clicked) && $('.pnsfiv').css({'color':'#FFD700'}) " )
+	{
+	  $('.pnsfour').css({'color':'#000'});	
+	  $('.pnsfiv').css({'color':'#000'});		
+	}	
+});
+
+$(".pnsfour").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	$(".pnsone").css({'color':'#FFD700'});
+	$(".pnstwo").css({'color':'#FFD700'});
+	$(".pnsthr").css({'color':'#FFD700'});
+	if("$('.pnsfour').data('clicked) && $('.pnsfiv').css({'color':'#FFD700'})" )
+	{
+	  $('.pnsfiv').css({'color':'#000'});	
+		
+	}	
+});
+
+$(".pnsfiv").on('click',function()	{
+	
+	
+	$(this).css({'color':'#FFD700'});
+	$(".pnsone").css({'color':'#FFD700'});
+	$(".pnstwo").css({'color':'#FFD700'});
+	$(".pnsthr").css({'color':'#FFD700'});
+	$(".pnsfour").css({'color':'#FFD700'});
+});
+
+
+
+//
+
+$(".wtone").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	if("$('.wtone').data('clicked) && $('.wttwo').css({'color':'#FFD700'}) || $('.wtone').data('clicked) && $('.wtthr').css({'color':'#FFD700'}) || $('.wtone').data('clicked) && $('.wtfour').css({'color':'#FFD700'}) || $('.wtone').data('clicked) && $('.wtfiv').css({'color':'#FFD700'}) ")
+	{
+	  $('.wttwo').css({'color':'#000'});	
+	  $('.wtthr').css({'color':'#000'});
+	  $('.wtfour').css({'color':'#000'});
+	  $('.wtfiv').css({'color':'#000'});
+	}
+	
+});
+
+$(".wttwo").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	
+	$(this).css({'color':'#FFD700'});
+	$(".wtone").css({'color':'#FFD700'});
+	if("$('.wttwo').data('clicked) && $('.wtthr').css({'color':'#FFD700'} ) || $('.wttwo').data('clicked) && $('.wtfour').css({'color':'#FFD700'}) || $('.wttwo').data('clicked) && $('.wtfiv').css({'color':'#FFD700'})" )
+	{
+	  $('.wtthr').css({'color':'#000'});	
+	  $('.wtfour').css({'color':'#000'});
+	  $('.wtfiv').css({'color':'#000'});	
+	}	
+	
+});
+
+$(".wtthr").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	$(".wtone").css({'color':'#FFD700'});
+	$(".wttwo").css({'color':'#FFD700'});
+	if("$('.wtthr').data('clicked) && $('.wtfour').css({'color':'#FFD700'}) || $('.wtthr').data('clicked) && $('.wtfiv').css({'color':'#FFD700'}) " )
+	{
+	  $('.wtfour').css({'color':'#000'});	
+	  $('.wtfiv').css({'color':'#000'});		
+	}	
+});
+
+$(".wtfour").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	$(".wtone").css({'color':'#FFD700'});
+	$(".wttwo").css({'color':'#FFD700'});
+	$(".wtthr").css({'color':'#FFD700'});
+	if("$('.wtfour').data('clicked) && $('.wtfiv').css({'color':'#FFD700'})" )
+	{
+	  $('.wtfiv').css({'color':'#000'});	
+		
+	}	
+});
+
+$(".wtfiv").on('click',function()	{
+	
+	
+	$(this).css({'color':'#FFD700'});
+	$(".wtone").css({'color':'#FFD700'});
+	$(".wttwo").css({'color':'#FFD700'});
+	$(".wtthr").css({'color':'#FFD700'});
+	$(".wtfour").css({'color':'#FFD700'});
+});
+
+
+
+
+
+//
+
+$(".cljone").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	if("$('.cljone').data('clicked) && $('.cljtwo').css({'color':'#FFD700'}) || $('.cljone').data('clicked) && $('.cljthr').css({'color':'#FFD700'}) || $('.cljone').data('clicked) && $('.cljfour').css({'color':'#FFD700'}) || $('.cljone').data('clicked) && $('.cljfiv').css({'color':'#FFD700'}) ")
+	{
+	  $('.cljtwo').css({'color':'#000'});	
+	  $('.cljthr').css({'color':'#000'});
+	  $('.cljfour').css({'color':'#000'});
+	  $('.cljfiv').css({'color':'#000'});
+	}
+	
+});
+
+$(".cljtwo").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	
+	$(this).css({'color':'#FFD700'});
+	$(".cljone").css({'color':'#FFD700'});
+	if("$('.cljtwo').data('clicked) && $('.cljthr').css({'color':'#FFD700'} ) || $('.cljtwo').data('clicked) && $('.cljfour').css({'color':'#FFD700'}) || $('.cljtwo').data('clicked) && $('.cljfiv').css({'color':'#FFD700'})" )
+	{
+	  $('.cljthr').css({'color':'#000'});	
+	  $('.cljfour').css({'color':'#000'});
+	  $('.cljfiv').css({'color':'#000'});	
+	}	
+	
+});
+
+$(".cljthr").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	$(".cljone").css({'color':'#FFD700'});
+	$(".cljtwo").css({'color':'#FFD700'});
+	if("$('.cljthr').data('clicked) && $('.cljfour').css({'color':'#FFD700'}) || $('.cljthr').data('clicked) && $('.cljfiv').css({'color':'#FFD700'}) " )
+	{
+	  $('.cljfour').css({'color':'#000'});	
+	  $('.cljfiv').css({'color':'#000'});		
+	}	
+});
+
+$(".cljfour").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	$(".cljone").css({'color':'#FFD700'});
+	$(".cljtwo").css({'color':'#FFD700'});
+	$(".cljthr").css({'color':'#FFD700'});
+	if("$('.cljfour').data('clicked) && $('.cljfiv').css({'color':'#FFD700'})" )
+	{
+	  $('.cljfiv').css({'color':'#000'});	
+		
+	}	
+});
+
+$(".cljfiv").on('click',function()	{
+	
+	
+	$(this).css({'color':'#FFD700'});
+	$(".cljone").css({'color':'#FFD700'});
+	$(".cljtwo").css({'color':'#FFD700'});
+	$(".cljthr").css({'color':'#FFD700'});
+	$(".cljfour").css({'color':'#FFD700'});
+});
+
+
+//
+$(".wjone").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	if("$('.wjone').data('clicked) && $('.wjtwo').css({'color':'#FFD700'}) || $('.wjone').data('clicked) && $('.wjthr').css({'color':'#FFD700'}) || $('.wjone').data('clicked) && $('.wjfour').css({'color':'#FFD700'}) || $('.wjone').data('clicked) && $('.wjfiv').css({'color':'#FFD700'}) ")
+	{
+	  $('.wjtwo').css({'color':'#000'});	
+	  $('.wjthr').css({'color':'#000'});
+	  $('.wjfour').css({'color':'#000'});
+	  $('.wjfiv').css({'color':'#000'});
+	}
+	
+});
+
+$(".wjtwo").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	
+	$(this).css({'color':'#FFD700'});
+	$(".wjone").css({'color':'#FFD700'});
+	if("$('.wjtwo').data('clicked) && $('.wjthr').css({'color':'#FFD700'} ) || $('.wjtwo').data('clicked) && $('.wjfour').css({'color':'#FFD700'}) || $('.wjtwo').data('clicked) && $('.wjfiv').css({'color':'#FFD700'})" )
+	{
+	  $('.wjthr').css({'color':'#000'});	
+	  $('.wjfour').css({'color':'#000'});
+	  $('.wjfiv').css({'color':'#000'});	
+	}	
+	
+});
+
+$(".wjthr").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	$(".wjone").css({'color':'#FFD700'});
+	$(".wjtwo").css({'color':'#FFD700'});
+	if("$('.wjthr').data('clicked) && $('.wjfour').css({'color':'#FFD700'}) || $('.wjthr').data('clicked) && $('.wjfiv').css({'color':'#FFD700'}) " )
+	{
+	  $('.wjfour').css({'color':'#000'});	
+	  $('.wjfiv').css({'color':'#000'});		
+	}	
+});
+
+$(".wjfour").on('click',function()	{
+	$(this).data('clicked',true);
+	
+	$(this).css({'color':'#FFD700'});
+	$(".wjone").css({'color':'#FFD700'});
+	$(".wjtwo").css({'color':'#FFD700'});
+	$(".wjthr").css({'color':'#FFD700'});
+	if("$('.wjfour').data('clicked) && $('.wjfiv').css({'color':'#FFD700'})" )
+	{
+	  $('.wjfiv').css({'color':'#000'});	
+		
+	}	
+});
+
+$(".wjfiv").on('click',function()	{
+	
+	
+	$(this).css({'color':'#FFD700'});
+	$(".wjone").css({'color':'#FFD700'});
+	$(".wjtwo").css({'color':'#FFD700'});
+	$(".wjthr").css({'color':'#FFD700'});
+	$(".wjfour").css({'color':'#FFD700'});
+});
+
+
+});
